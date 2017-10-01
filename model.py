7e9483e988eb904b52a920c04a669b6b27c93a1f
George Dzhgarkava <dg@roommediagroup.com>
2017-10-01 18:56:09 +0100

    Added model.py

---
+++ model.py
@@ -0,0 +1,81 @@
+import csv
+import cv2
+import numpy as np
+import sklearn
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Convolution2D
+
+from sklearn.model_selection import train_test_split
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                
+                for i in range(3):
+                    source_path = batch_sample[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = 'data/IMG/' + filename
+                    image = cv2.imread(current_path)
+                    images.append(image)
+                    correction = 0
+                    if (i == 1):
+                        correction = 0.2
+                    if (i == 2):
+                        correction = -0.2
+                    measurement = float(line[3])+correction
+                    measurements.append(measurement)
+                    
+                    # Added augmented images and measurements
+                    images.append(cv2.flip(image,1))
+                    measurements.append(measurement * -1.0)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+lines = []
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+# Create train set and validation set
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+
+# Compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=64)
+validation_generator = generator(validation_samples, batch_size=64)
+
+# Model
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit_generator(train_generator,
+                    samples_per_epoch=len(train_samples)*6, 
+                    validation_data=validation_generator, 
+                    nb_val_samples=len(validation_samples)*6, 
+                    nb_epoch=7)
+
+model.save('model.h5')
